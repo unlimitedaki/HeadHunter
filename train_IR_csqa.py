a8d4ac11b46661f86aa9f50045f286bb3beeb5bc
unlimitedaki <unlimitedaki@gmail.com>
2020-09-23 08:15:48 +0000

    TPU！

--- train_IR_csqa.py
+++ train_IR_csqa.py
@@ -8,7 +8,7 @@ import json
 from transformers import BertModel,BertTokenizer,AlbertTokenizer,RobertaTokenizer,XLNetTokenizer
 from tqdm import tqdm
 import logging
-from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset
+from torch.utils.data import DataLoader, RandomSampler, SequentialSampler, TensorDataset, DistributedSampler
 import argparse
 import time
 import torch.nn as nn
@@ -19,6 +19,8 @@ import numpy as np
 from processor import *
 from model import *
 from apex import amp
+
+
 
 def clean_data(cs_str):
     if "The statement " in cs_str:
@@ -86,49 +88,76 @@ def select_model(args):
         
 
 
-def train(args):
+def train(index,args,train_dataset,dev_examples,dev_dataset,logger,omcs_corpus):
     # setup output dir for model and log
-    output_dir = os.path.join(args.output_dir,args.save_model_name)
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    if not args.tpu:
+        output_dir = os.path.join(args.output_dir,args.save_model_name)
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
 
-    arg_dict = args.__dict__
-    with open(os.path.join(output_dir,"args.json"),'w',encoding='utf8') as f:
-        json.dump(arg_dict,f,indent=2,ensure_ascii=False)
+        arg_dict = args.__dict__
+        with open(os.path.join(output_dir,"args.json"),'w',encoding='utf8') as f:
+            json.dump(arg_dict,f,indent=2,ensure_ascii=False)
+            
+        # setup logging
+        logfilename = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+" "+args.save_model_name+".log.txt"
+        fh = logging.FileHandler(os.path.join(output_dir,logfilename), mode='a', encoding='utf-8')
+        fh.setLevel(logging.INFO)
+        logger.addHandler(fh)
+
+        # setup seed
+        np.random.seed(args.seed)
+        torch.manual_seed(args.seed)
+        if torch.cuda.is_available():
+            torch.cuda.manual_seed_all(args.seed)
+
+        tokenizer = select_tokenizer(args)
+
+        # load data
+        omcs_corpus = load_omcs(args)
+        if args.cs_len > 0:
+            # omcs_corpus = None
+            
+            _,_,train_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"train")
+            dev_examples,_,dev_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"dev")
+            # _,_,test_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"test",is_training = False)
+
+        else:
+            _,_,train_dataset= load_csqa_dataset(tokenizer,args,"train")
+            dev_examples,_,dev_dataset= load_csqa_dataset(tokenizer,args,"dev")
+            # _,_,test_dataset= load_csqa_dataset(tokenizer,args,"test",is_training = False)
+    if args.tpu:
+        # use tpu
+        import torch_xla
+        import torch_xla.core.xla_model as xm
+        import torch_xla.distributed.parallel_loader as pl
+        device = xm.xla_device()
         
-    # setup logging
-    logfilename = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+" "+args.save_model_name+".log.txt"
-    fh = logging.FileHandler(os.path.join(output_dir,logfilename), mode='a', encoding='utf-8')
-    fh.setLevel(logging.INFO)
-    logger.addHandler(fh)
-
-    # setup seed
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
-    if torch.cuda.is_available():
-        torch.cuda.manual_seed_all(args.seed)
-
-    tokenizer = select_tokenizer(args)
-
-    # load data
-    omcs_corpus = load_omcs(args)
-    if args.cs_len > 0:
-        # omcs_corpus = None
-        
-        _,_,train_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"train")
-        dev_examples,_,dev_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"dev")
-        # _,_,test_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"test",is_training = False)
-
+        train_sampler = DistributedSampler(
+            train_dataset,
+            num_replicas=xm.xrt_world_size(),
+            rank=xm.get_ordinal(),
+            shuffle=True)
+        dev_sampler = DistributedSampler(
+            dev_dataset,
+            num_replicas=xm.xrt_world_size(),
+            rank=xm.get_ordinal(),
+            shuffle=False)
+        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+        train_size = len(train_dataloader)
+        t_total = train_size // args.gradient_accumulation_steps * args.num_train_epochs
+        dev_dataloader = DataLoader(dev_dataset, sampler=dev_sampler, batch_size=args.eval_batch_size)
+        train_dataloader = pl.ParallelLoader(train_dataloader, [device]).per_device_loader(device)
+        dev_dataloader = pl.ParallelLoader(dev_dataloader, [device]).per_device_loader(device)
+        print("Process", index ,"is using", xm.xla_real_devices([str(device)])[0])
     else:
-        _,_,train_dataset= load_csqa_dataset(tokenizer,args,"train")
-        dev_examples,_,dev_dataset= load_csqa_dataset(tokenizer,args,"dev")
-        # _,_,test_dataset= load_csqa_dataset(tokenizer,args,"test",is_training = False)
-    
-    train_sampler = RandomSampler(train_dataset) 
-    train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+        device = torch.device('cuda:0')
 
-    dev_sampler = SequentialSampler(dev_dataset) 
-    dev_dataloader = DataLoader(dev_dataset, sampler=dev_sampler, batch_size=args.eval_batch_size)
+        train_sampler = RandomSampler(train_dataset) 
+        train_dataloader = DataLoader(train_dataset, sampler=train_sampler, batch_size=args.train_batch_size)
+        t_total = len(train_dataloader) // args.gradient_accumulation_steps * args.num_train_epochs
+        dev_sampler = SequentialSampler(dev_dataset) 
+        dev_dataloader = DataLoader(dev_dataset, sampler=dev_sampler, batch_size=args.eval_batch_size)
 
     # test_sampler = SequentialSampler(test_dataset) 
     # test_dataloader = DataLoader(test_dataset, sampler=test_sampler, batch_size=args.eval_batch_size)
@@ -149,11 +178,10 @@ def train(args):
         status['best_Acc'] = 0.0
         status['current_epoch']  = 0
         
-    device = torch.device('cuda:0')
     model.to(device)
 
     # Prepare optimizer and schedule (linear warmup and decay)
-    t_total = len(train_dataloader) // args.gradient_accumulation_steps * args.num_train_epochs
+    
     no_decay = ["bias", "LayerNorm.weight"]
     optimizer_grouped_parameters = [
         {
@@ -183,13 +211,6 @@ def train(args):
         for step, batch in enumerate(epoch_iterator):
             model.train()
             batch = tuple(t.to(device) for t in batch)
-            # if 'roberta' in args.origin_model:
-            #     inputs = {
-            #     "input_ids": batch[0],
-            #     "attention_mask": batch[1],
-            #     "labels": batch[2]
-            #     }
-            # else:
             inputs = {
                 "input_ids": batch[0],
                 "attention_mask": batch[1],
@@ -209,11 +230,14 @@ def train(args):
             tr_loss += loss.item()
             if (step + 1) % args.gradient_accumulation_steps == 0:
                 torch.nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
-                optimizer.step()
+                if args.tpu:
+                    xm.optimizer_step(optimizer)
+                else:
+                    optimizer.step()
                 scheduler.step() 
                 model.zero_grad()
             # check average training loss
-            if (step + 1)% args.check_loss_step == 0 or step == len(train_dataloader):
+            if (step + 1)% args.check_loss_step == 0:
                 avg_loss = (tr_loss/(step+1)) * args.gradient_accumulation_steps
                 logger.info("\t average_step_loss=%s @ step = %s on epoch = %s",str(avg_loss),str(step+1),str(epoch+1))
         # Eval : one time one epoch
@@ -257,6 +281,48 @@ def train(args):
         # save status
         status_dir = os.path.join(output_dir,"status.json")
         json.dump(status,open(status_dir,'w',encoding = 'utf8'))
+
+def tpu_training(args):
+
+    import torch_xla
+    import torch_xla.core.xla_model as xm
+    import torch_xla.distributed.xla_multiprocessing as xmp
+    output_dir = os.path.join(args.output_dir,args.save_model_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    arg_dict = args.__dict__
+    with open(os.path.join(output_dir,"args.json"),'w',encoding='utf8') as f:
+        json.dump(arg_dict,f,indent=2,ensure_ascii=False)
+        
+    # setup logging
+    logfilename = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())+" "+args.save_model_name+".log.txt"
+    fh = logging.FileHandler(os.path.join(output_dir,logfilename), mode='a', encoding='utf-8')
+    fh.setLevel(logging.INFO)
+    logger.addHandler(fh)
+
+    # setup seed
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(args.seed)
+
+    tokenizer = select_tokenizer(args)
+
+    # load data
+    omcs_corpus = load_omcs(args)
+    if args.cs_len > 0:
+        # omcs_corpus = None
+        
+        _,_,train_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"train")
+        dev_examples,_,dev_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"dev")
+        # _,_,test_dataset= load_csqa_omcs_dataset(tokenizer,args,omcs_corpus,"test",is_training = False)
+
+    else:
+        _,_,train_dataset= load_csqa_dataset(tokenizer,args,"train")
+        dev_examples,_,dev_dataset= load_csqa_dataset(tokenizer,args,"dev")
+        # _,_,test_dataset= load_csqa_dataset(tokenizer,args,"test",
+    xmp.spawn(train,args = (args,train_dataset,dev_examples,dev_dataset,logger,omcs_corpus), nprocs=8, start_method='fork')
 
 def make_predictions(args,examples,predictions,omcs_corpus,data_type="dev"):
   cs_file = "OMCS/{}_{}_omcs_of_dataset.json".format(data_type,args.cs_mode)
@@ -345,14 +411,18 @@ if __name__ == "__main__":
     parser.add_argument("--cs_len",type = int, default = 5)
     # settings
     parser.add_argument("--n_gpu",type=int , default = 1)
-    parser.add_argument("--fp16",type =bool,default = False)
+    parser.add_argument("--fp16",action = "store_true")
     parser.add_argument("--save_method",type = str,default = "Best_Current")
     parser.add_argument("--do_finetune",action = "store_true",default = False)
     parser.add_argument("--cs_mode",type = str,default = "wholeQA-Match")
     parser.add_argument("--cs_save_mode",type = str,default = "id")
     parser.add_argument("--seed",type = int,default = 1,help = "freeze seed")
+    parser.add_argument('--tpu',action = "store_true")
+
 
     # args = parser.parse_args() 在notebook 里 args 需要初始化为[],外部调用py文件不需要
     args = parser.parse_args()
-
-    train(args)
+    if args.tpu:
+        tpu_training(args)
+    else:
+        train(args)
