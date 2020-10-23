ceb8920e55e98455038efd66ebe2381c5ebd3777
Aki <unlimitedaki@gmail.com>
2020-10-23 18:06:33 +0800

    fix model.py

--- model.py
+++ model.py
@@ -18,7 +18,6 @@ class BertForMultipleChoice(BertPreTrainedModel):
 
         self.init_weights()
 
-# @add_start_docstrings_to_callable(BERT_INPUTS_DOCSTRING.format("(batch_size, num_choices, sequence_length)"))
     def forward(
         self,
         input_ids=None,
@@ -68,13 +67,12 @@ class AlbertForMultipleChoice(AlbertPreTrainedModel):
     def __init__(self, config):
         super().__init__(config)
 
-        self.bert = AlbertModel(config)
+        self.albert = AlbertModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, 1)
 
         self.init_weights()
 
-# @add_start_docstrings_to_callable(BERT_INPUTS_DOCSTRING.format("(batch_size, num_choices, sequence_length)"))
     def forward(
         self,
         input_ids=None,
@@ -84,9 +82,11 @@ class AlbertForMultipleChoice(AlbertPreTrainedModel):
         head_mask=None,
         inputs_embeds=None,
         labels=None,
-        output_attentions=False,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
     ):
-        
+        # return_dict = return_dict if return_dict is not None else self.config.use_return_dict
         num_choices = input_ids.shape[1] if input_ids is not None else inputs_embeds.shape[1]
 
         input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
@@ -98,126 +98,70 @@ class AlbertForMultipleChoice(AlbertPreTrainedModel):
             if inputs_embeds is not None
             else None
         )
-
-        outputs = self.bert(
+        outputs = self.albert(
             input_ids,
             attention_mask=attention_mask,
-            token_type_ids=token_type_ids
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds
         )
-
         pooled_output = outputs[1]
 
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         reshaped_logits = logits.view(-1, num_choices)
 
-        outputs = (reshaped_logits,) + outputs[2:]  # add hidden states and attention if they are here
-
+        loss = None
         if labels is not None:
             loss_fct = CrossEntropyLoss()
             loss = loss_fct(reshaped_logits, labels)
-            outputs = (loss,) + outputs
+        output = (reshaped_logits,) + outputs[2:]
+        return ((loss,) + output) if loss is not None else output
 
-        return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
-
-# class RobertaForMultipleChoice(BertPreTrainedModel):
-#     config_class = RobertaConfig
-#     base_model_prefix = "roberta"
-
-#     def __init__(self, config):
-#         super().__init__(config)
-
-#         self.roberta = RobertaModel(config)
-#         self.dropout = nn.Dropout(config.hidden_dropout_prob)
-#         self.classifier = nn.Linear(config.hidden_size, 1)
-
-#         self.init_weights()
-
-#     def forward(
-#         self,
-#         input_ids=None,
-#         token_type_ids=None,
-#         attention_mask=None,
-#         labels=None,
-#         position_ids=None,
-#         head_mask=None,
-#         inputs_embeds=None,
-#     ):
-#         num_choices = input_ids.shape[1]
-
-#         flat_input_ids = input_ids.view(-1, input_ids.size(-1))
-#         flat_position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
-#         flat_token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
-#         flat_attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
-#         outputs = self.roberta(
-#             flat_input_ids,
-#             position_ids=flat_position_ids,
-#             token_type_ids=flat_token_type_ids,
-#             attention_mask=flat_attention_mask,
-#             head_mask=head_mask,
-#         )
-#         pooled_output = outputs[1]
-
-#         pooled_output = self.dropout(pooled_output)
-#         logits = self.classifier(pooled_output)
-#         reshaped_logits = logits.view(-1, num_choices)
-
-#         outputs = (reshaped_logits,) + outputs[2:]  # add hidden states and attention if they are here
-
-#         if labels is not None:
-#             loss_fct = CrossEntropyLoss()
-#             loss = loss_fct(reshaped_logits, labels)
-#             outputs = (loss,) + outputs
-
-#         return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
 
 class RobertaForMultipleChoice(BertPreTrainedModel):
+    # config_class = RobertaConfig
+    # base_model_prefix = "roberta"
+
     def __init__(self, config):
         super().__init__(config)
 
-        self.bert = RobertaModel(config)
+        self.roberta = RobertaModel(config)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.classifier = nn.Linear(config.hidden_size, 1)
 
         self.init_weights()
 
-# @add_start_docstrings_to_callable(BERT_INPUTS_DOCSTRING.format("(batch_size, num_choices, sequence_length)"))
     def forward(
         self,
         input_ids=None,
-        attention_mask=None,
         token_type_ids=None,
+        attention_mask=None,
+        labels=None,
         position_ids=None,
         head_mask=None,
         inputs_embeds=None,
-        labels=None,
-        output_attentions=False,
     ):
-        # pdb.set_trace()
-        num_choices = input_ids.shape[1] if input_ids is not None else inputs_embeds.shape[1]
+        num_choices = input_ids.shape[1]
 
-        input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
-        attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
-        # token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
-        token_type_ids = None
-        position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
-        inputs_embeds = (
-            inputs_embeds.view(-1, inputs_embeds.size(-2), inputs_embeds.size(-1))
-            if inputs_embeds is not None
-            else None
+        flat_input_ids = input_ids.view(-1, input_ids.size(-1))
+        flat_position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+        flat_token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        flat_attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        outputs = self.roberta(
+            flat_input_ids,
+            position_ids=flat_position_ids,
+            token_type_ids=flat_token_type_ids,
+            attention_mask=flat_attention_mask,
+            head_mask=head_mask,
         )
-        # pdb.set_trace()
-        outputs = self.bert(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids
-        )
-
         pooled_output = outputs[1]
+
         pooled_output = self.dropout(pooled_output)
         logits = self.classifier(pooled_output)
         reshaped_logits = logits.view(-1, num_choices)
-        # pdb.set_trace()
+
         outputs = (reshaped_logits,) + outputs[2:]  # add hidden states and attention if they are here
 
         if labels is not None:
@@ -226,8 +170,6 @@ class RobertaForMultipleChoice(BertPreTrainedModel):
             outputs = (loss,) + outputs
 
         return outputs  # (loss), reshaped_logits, (hidden_states), (attentions)
-
-
 
 class XLNetForMultipleChoice(XLNetPreTrainedModel):
     def __init__(self, config):
@@ -350,18 +292,8 @@ class BertAttRanker(BertPreTrainedModel):
         )
 
         pooled_output = bert_outputs[1]
-        
         reshaped_output = pooled_output.view(int(batch_size*num_choices),self.cs_len,pooled_output.size(-1))
-
         atten_output = self.self_att(reshaped_output)
-        # pdb.set_trace()
-        # mean_pooled_output = atten_output.mean(dim=1)
-
-        # mean_pooled_output = self.dropout(mean_pooled_output)
-        
-        # logits = self.classifier(mean_pooled_output)
-
-
         reshaped_output = atten_output.view(int(batch_size*num_choices),self.cs_len*atten_output.size(-1))
         logits = self.classifier(reshaped_output)
         reshaped_logits = logits.view(-1, num_choices)
@@ -379,14 +311,17 @@ class AlbertAttRanker(AlbertPreTrainedModel):
     def __init__(self, config, cs_len):
         super().__init__(config)
         self.cs_len = cs_len
-        self.bert = AlbertModel(config)
+        self.albert = AlbertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.self_att = SelfAttention(config)
-        # self.classifier = nn.Linear(config.hidden_size,1)
         self.classifier = nn.Linear(config.hidden_size*self.cs_len,1)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.init_weights()
 
-    def forward(self,
+        self.init_weights()
+
+    def forward(
+        self,
         input_ids=None,
         attention_mask=None,
         token_type_ids=None,
@@ -394,10 +329,14 @@ class AlbertAttRanker(AlbertPreTrainedModel):
         head_mask=None,
         inputs_embeds=None,
         labels=None,
-        output_attentions=False,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
     ):
         batch_size,input_size = input_ids.shape[:2]
         num_choices = int(input_size/self.cs_len)
+        # num_choices = input_ids.shape[1] if input_ids is not None else inputs_embeds.shape[1]
+
         input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
         attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
         token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
@@ -407,26 +346,20 @@ class AlbertAttRanker(AlbertPreTrainedModel):
             if inputs_embeds is not None
             else None
         )
-        # pdb.set_trace()
-
-        bert_outputs = self.bert(
+        outputs = self.albert(
             input_ids,
             attention_mask=attention_mask,
-            token_type_ids=token_type_ids
+            token_type_ids=token_type_ids,
+            position_ids=position_ids,
+            head_mask=head_mask,
+            inputs_embeds=inputs_embeds
         )
-
-        pooled_output = bert_outputs[1]
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
         
         reshaped_output = pooled_output.view(int(batch_size*num_choices),self.cs_len,pooled_output.size(-1))
 
         atten_output = self.self_att(reshaped_output)
-        # pdb.set_trace()
-        # mean_pooled_output = atten_output.mean(dim=1)
-
-        # mean_pooled_output = self.dropout(mean_pooled_output)
-        
-        # logits = self.classifier(mean_pooled_output)
-
 
         reshaped_output = atten_output.view(int(batch_size*num_choices),self.cs_len*atten_output.size(-1))
         logits = self.classifier(reshaped_output)
@@ -445,9 +378,8 @@ class RobertaAttRanker(BertPreTrainedModel):
     def __init__(self, config, cs_len):
         super().__init__(config)
         self.cs_len = cs_len
-        self.bert = RobertaModel(config)
+        self.roberta = RobertaModel(config)
         self.self_att = SelfAttention(config)
-        # self.classifier = nn.Linear(config.hidden_size,1)
         self.classifier = nn.Linear(config.hidden_size*self.cs_len,1)
         self.dropout = nn.Dropout(config.hidden_dropout_prob)
         self.init_weights()
@@ -464,35 +396,24 @@ class RobertaAttRanker(BertPreTrainedModel):
     ):
         batch_size,input_size = input_ids.shape[:2]
         num_choices = int(input_size/self.cs_len)
-        input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
-        attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
-        token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
-        position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
-        inputs_embeds = (
-            inputs_embeds.view(-1, inputs_embeds.size(-2), inputs_embeds.size(-1))
-            if inputs_embeds is not None
-            else None
-        )
-        # pdb.set_trace()
-
-        bert_outputs = self.bert(
-            input_ids,
-            attention_mask=attention_mask,
-            token_type_ids=token_type_ids
+        flat_input_ids = input_ids.view(-1, input_ids.size(-1))
+        flat_position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+        flat_token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        flat_attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        outputs = self.roberta(
+            flat_input_ids,
+            position_ids=flat_position_ids,
+            token_type_ids=flat_token_type_ids,
+            attention_mask=flat_attention_mask,
+            head_mask=head_mask,
         )
 
-        pooled_output = bert_outputs[1]
+        pooled_output = outputs[1]
+        pooled_output = self.dropout(pooled_output)
         
         reshaped_output = pooled_output.view(int(batch_size*num_choices),self.cs_len,pooled_output.size(-1))
 
         atten_output = self.self_att(reshaped_output)
-        # pdb.set_trace()
-        # mean_pooled_output = atten_output.mean(dim=1)
-
-        # mean_pooled_output = self.dropout(mean_pooled_output)
-        
-        # logits = self.classifier(mean_pooled_output)
-
 
         reshaped_output = atten_output.view(int(batch_size*num_choices),self.cs_len*atten_output.size(-1))
         logits = self.classifier(reshaped_output)
@@ -552,7 +473,6 @@ class XLNetAttRanker(XLNetPreTrainedModel):
         reshaped_output = pooled_output.view(int(batch_size*num_choices),self.cs_len,pooled_output.size(-1))
 
         atten_output = self.self_att(reshaped_output)
-
 
         reshaped_output = atten_output.view(int(batch_size*num_choices),self.cs_len*atten_output.size(-1))
         logits = self.classifier(reshaped_output)
