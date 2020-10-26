7b52cd737a438c8447fad08f14a43c0e5b503559
unlimitedaki <unlimitedaki@gmail.com>
2020-10-26 02:51:57 +0000

    don't rank

--- model.py
+++ model.py
@@ -387,6 +387,61 @@ class BertAttRankerPRA(BertPreTrainedModel):
 
         return outputs
 
+class BertAttRankerDontRank(BertPreTrainedModel):
+    def __init__(self, config, cs_len):
+        super().__init__(config)
+        self.cs_len = cs_len
+        self.bert = BertModel(config)
+        # self.self_att = SelfAttention(config) don't atten
+        # self.classifier = nn.Linear(config.hidden_size,1)
+        self.classifier = nn.Linear(config.hidden_size*self.cs_len,1)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        self.init_weights()
+
+    def forward(self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=False,
+    ):
+        batch_size,input_size = input_ids.shape[:2]
+        num_choices = int(input_size/self.cs_len)
+        input_ids = input_ids.view(-1, input_ids.size(-1)) if input_ids is not None else None
+        attention_mask = attention_mask.view(-1, attention_mask.size(-1)) if attention_mask is not None else None
+        token_type_ids = token_type_ids.view(-1, token_type_ids.size(-1)) if token_type_ids is not None else None
+        position_ids = position_ids.view(-1, position_ids.size(-1)) if position_ids is not None else None
+        inputs_embeds = (
+            inputs_embeds.view(-1, inputs_embeds.size(-2), inputs_embeds.size(-1))
+            if inputs_embeds is not None
+            else None
+        )
+
+        bert_outputs = self.bert(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids
+        )
+
+        pooled_output = bert_outputs[1] # (bz*num_choice*cslen,hz)
+        reshaped_output = pooled_output.view(int(batch_size*num_choices),self.cs_len,pooled_output.size(-1))
+        # atten_output = self.self_att(reshaped_output)
+        reshaped_output = reshaped_output.view(int(batch_size*num_choices),self.cs_len*atten_output.size(-1))
+        logits = self.classifier(reshaped_output)
+        reshaped_logits = logits.view(-1, num_choices)
+
+        outputs = (reshaped_logits,)
+
+        if labels is not None:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(reshaped_logits, labels)
+            outputs = (loss,) + outputs
+
+        return outputs
+
 class AlbertAttRanker(AlbertPreTrainedModel):
     def __init__(self, config, cs_len):
         super().__init__(config)
--- train.py
+++ train.py
@@ -86,6 +86,9 @@ def select_model(args):
             return BertAttRanker.from_pretrained(args.origin_model,cache_dir = cache,cs_len = args.cs_len)
         elif "xlnet" in args.origin_model:
             return XLNetAttRanker.from_pretrained(args.origin_model,cache_dir = cache,cs_len = args.cs_len)
+    elif args.task_name == "rerank_csqa_without_rank":
+        if "bert" in args.origin_model:
+            return BertAttRankerDontRank.from_pretrained(args.origin_model,cache_dir = cache,cs_len = args.cs_len)
     else:
         if "albert" in args.origin_model:
             return AlbertForMultipleChoice.from_pretrained(args.origin_model,cache_dir = cache)
