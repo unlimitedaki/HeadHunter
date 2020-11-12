73a5d7d189176edc9a3ecd96a361b6d30f9aa67a
Zaaachary <li_zaaachary@163.com>
2020-11-12 07:10:36 +0000

    hingeloss debug

--- HingeLoss.py
+++ HingeLoss.py
@@ -1,3 +1,5 @@
+import pdb
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,18 +11,20 @@ class HingeLoss(nn.Module):
         self.margin = margin
 
     def forward(self, reshaped_logits, labels):
-        # logits 过 softmax
-        reshaped_logits = F.softmax(reshaped_logits, dim=1)
-        # onehot 编码的labels  [batch, 5]
-        oh_labels = F.one_hot(labels).squeeze(1)
+        # 分数过 softmax
+        reshaped_logits = F.softmax(reshaped_logits, dim=1)  # [batch, 5]
+        # 得到 label 的 onehot
+        oh_labels = F.one_hot(labels,num_classes=5).squeeze(1)  # [batch, 5]
         mask = oh_labels.bool()
-        # 利用 mask 选出正例 [batch, 1]
-        logits_p = reshaped_logits.masked_select(mask).reshape(-1, 1)
-        logits_p.repeat(1, 5)                   # [batch,1] -> [batch,5]
+        # pdb.set_trace()
+        # 利用 mask 选出正例 
+        logits_p = reshaped_logits.masked_select(mask)
+        logits_p = logits_p.reshape(-1, 1)  # [batch, 1]
+        logits_p = logits_p.repeat(1, 5)                   # [batch,1] -> [batch,5]
         logits_n = reshaped_logits
         logits_pn = logits_p - logits_n         # pos - neg
         oh_labels = oh_labels * 2 - 1           # 0, 1  -> -1, 1
 
         Loss = nn.HingeEmbeddingLoss(self.margin)
-        return Loss(logits_pn, oh_labels*2-1)
+        return Loss(logits_pn, oh_labels)
 
