502c8cf8e4e548e755393b3d94b05f8ab11110e9
unlimitedaki <unlimitedaki@gmail.com>
2020-12-01 11:25:14 +0000

    batch dev

--- dev_with_diff_cs-len.sh
+++ dev_with_diff_cs-len.sh
@@ -3,13 +3,13 @@ for i in {1..20}
 do
     python train.py \
         --task_name "rerank_csqa" \
-        --save_model_name $1 \
+        --save_model_name $2 \
         --origin_model "bert-base-cased" \
         --cs_mode "QAconcept-Match" \
         --learning_rate 2e-5 \
         --cs_len 7 \
         --dev_cs_len $i \
-        --output_dir "/content/drive/Shareddrives/lab/rerank_CSQA/model_weight_sum/" \
+        --output_dir $1 \
         --num_train_epochs 5 \
         --train_batch_size 4 \
         --eval_batch_size 2 \
