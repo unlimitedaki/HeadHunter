5deac021267a5bf144ee4453ae7c2d8ace9c7c54
unlimitedaki <unlimitedaki@gmail.com>
2020-12-16 12:46:14 +0000

    add more parameters to dev script

--- dev_with_diff_cs-len.sh
+++ dev_with_diff_cs-len.sh
@@ -3,13 +3,13 @@ for i in {1..20}
 do
     python train.py \
         --task_name $1 \
-        --save_model_name $3 \
-        --origin_model "bert-base-cased" \
+        --save_model_name $4 \
+        --origin_model $2 \
         --cs_mode "QAconcept-Match" \
         --learning_rate 2e-5 \
         --cs_len 7 \
         --dev_cs_len $i \
-        --output_dir $2 \
+        --output_dir $3 \
         --num_train_epochs 5 \
         --train_batch_size 4 \
         --eval_batch_size 2 \
